7e69937559d4f2dbf5fe7954ab53300f905b6cd8
Brais Cidras <braisomary@gmail.com>
2021-11-13 21:25:02 +0100

    Build Our Own HTTP Observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { noop, Observable} from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,28 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const http$ = new Observable(
+      observer => {
+        // Getting courses through a promise
+        fetch('/api/courses')
+          .then(response => {
+            return response.json();
+          })
+          .then(body => {
+            observer.next(body);
+            observer.complete();
+          })
+          .catch(err => {
+            observer.error(err);
+          })
+      }
+    );
+
+    http$.subscribe(
+      courses => console.log(courses),
+      noop, // No operation. Replaces the empty callback () => {}
+      () => console.log('completed')
+    );
   }
 
 }
