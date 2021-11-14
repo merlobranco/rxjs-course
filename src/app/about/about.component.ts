95690f36540c92673f9f0aaab60ca90004e4e13c
Brais Cidras <braisomary@gmail.com>
2021-11-14 17:08:45 +0100

    Observable Concatenation - Uncompleted exmaple

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { concat, of } from 'rxjs';
+import { concat, interval, of } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -11,7 +11,8 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const source1$ = of(1, 2, 3);
+    const source1$ = interval(1000);
+    // The value of source2$ never will be added since the source1$ is never completed
     const source2$ = of(4, 5, 6);
     const source3$ = of(7, 8, 9);
 
