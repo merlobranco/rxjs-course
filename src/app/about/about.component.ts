68964568932680117243c68280636f1af1522d9a
Brais Cidras <braisomary@gmail.com>
2021-11-14 18:48:08 +0100

    The merge Observable combination Strategy

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { concat, interval, of } from 'rxjs';
+import { concat, interval, merge, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -11,14 +12,12 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    // const source1$ = interval(1000);
-    const source1$ = of(1, 2, 3);
-    // The value of source2$ never will be added since the source1$ is never completed
-    const source2$ = of(4, 5, 6);
-    const source3$ = of(7, 8, 9);
+    const interval1$ = interval(1000)
 
-    const result$ = concat(source1$, source2$, source3$);
+    const interva2$ = interval1$.pipe(map(val => val * 10));
 
-    result$.subscribe(console.log)
+    const result$ = merge(interval1$, interva2$);
+
+    result$.subscribe(console.log);
   }
 }
