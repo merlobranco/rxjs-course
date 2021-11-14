4ee95493ce668d9823ea98f4681132fab511537f
Brais Cidras <braisomary@gmail.com>
2021-11-14 18:06:46 +0100

    The RxJs concatMap Operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,7 +11,8 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const source1$ = interval(1000);
+    // const source1$ = interval(1000);
+    const source1$ = of(1, 2, 3);
     // The value of source2$ never will be added since the source1$ is never completed
     const source2$ = of(4, 5, 6);
     const source3$ = of(7, 8, 9);
--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -3,7 +3,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import {Course} from "../model/course";
 import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 import * as moment from 'moment';
-import {fromEvent} from 'rxjs';
+import {concat, fromEvent} from 'rxjs';
 import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap} from 'rxjs/operators';
 import {fromPromise} from 'rxjs/internal-compatibility';
 
@@ -14,6 +14,7 @@ import {fromPromise} from 'rxjs/internal-compatibility';
 })
 export class CourseDialogComponent implements OnInit, AfterViewInit {
 
+    // Angular provides at the level of the form an Observable that emits the values that are contain in the form
     form: FormGroup;
     course:Course;
 
@@ -38,12 +39,25 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
     }
 
     ngOnInit() {
-
-
-
+        this.form.valueChanges
+            .pipe(
+                filter(() => this.form.valid),
+                // We want to guarantee the values are saved in the same order as they are emmitted
+                // Our safe operations are happening in sequence in the same order we want to implement them
+                concatMap(changes => this.saveCourse(changes))
+            )
+            .subscribe()
     }
 
-
+    async saveCourse(changes) {
+        fromPromise(fetch(`/api/courses/${this.course.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(changes),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }));
+    }
 
     ngAfterViewInit() {
 
