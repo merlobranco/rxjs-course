eeeb56e9db349c6887c8e65e06d9999eaf818639
Brais Cidras <braisomary@gmail.com>
2021-11-14 17:04:42 +0100

    Observable Concatenation - In-Depth Explanation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { noop } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { createHttpObservable } from '../common/util';
+import { concat, of } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -13,5 +11,12 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const source1$ = of(1, 2, 3);
+    const source2$ = of(4, 5, 6);
+    const source3$ = of(7, 8, 9);
+
+    const result$ = concat(source1$, source2$, source3$);
+
+    result$.subscribe(console.log)
   }
 }
