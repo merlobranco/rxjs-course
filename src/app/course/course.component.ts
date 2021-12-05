400748ce2d34f15090c67e2332bf92b64c5e0463
Brais Cidras <braisomary@gmail.com>
2021-12-05 18:17:21 +0100

    The RxJs forkJoin Operator

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,23 +1,17 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
-import {Course} from "../model/course";
 import {
     debounceTime,
     distinctUntilChanged,
     startWith,
     tap,
-    delay,
     map,
-    concatMap,
-    switchMap,
-    withLatestFrom,
-    concatAll, shareReplay
+    switchMap
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
+import {fromEvent, Observable, concat, forkJoin} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
-import { searchLessons } from '../../../server/search-lessons.route';
-import {  debug, RxJsLoggingLevel, setRxjsLoggingLevel } from '../common/debug';
+import { Course } from '../model/course';
 
 
 @Component({
@@ -40,12 +34,18 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
         this.courseId = this.route.snapshot.params['id'];
-        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`)
-            .pipe(
-                debug(RxJsLoggingLevel.INFO, "course value")
-            );
+        // We want to send this http request at the same time to the backend in parallel and wait for the results of both of them
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+        const lessons$ = this.loadLessons();
 
-        setRxjsLoggingLevel(RxJsLoggingLevel.DEBUG);
+        forkJoin(this.course$, lessons$)
+            .pipe(
+                tap(([course, lessons]) => {
+                    console.log('course', course);
+                    console.log('lessons', lessons)
+                })
+            )
+            .subscribe()
     }
 
     ngAfterViewInit() {
@@ -53,11 +53,9 @@ export class CourseComponent implements OnInit, AfterViewInit {
             .pipe(
                 map(event => (<HTMLInputElement>(<Event>event).target).value),
                 startWith(''), // It will provide an initial value of string empry to the search parameter
-                debug(RxJsLoggingLevel.TRACE, "search"),
                 debounceTime(400), // Value provided in milliseconds
                 distinctUntilChanged(),
-                switchMap(search => this.loadLessons(search)),
-                debug(RxJsLoggingLevel.DEBUG, "lessons value"),
+                switchMap(search => this.loadLessons(search))
             );
     }
 
