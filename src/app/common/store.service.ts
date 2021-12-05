1a4e1b381c75459f0ef737fee28613308a80c66f
Brais Cidras <braisomary@gmail.com>
2021-12-05 20:48:26 +0100

    BehaviorSubject Store - Example of a Data Modification Operation

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable, timer } from "rxjs";
+import { fromPromise } from "rxjs/internal-compatibility";
 import { tap, map } from "rxjs/operators";
 import { Course } from "../model/course";
 import { createHttpObservable } from "./util";
@@ -37,5 +38,31 @@ export class Store {
         return this.courses$.pipe(
             map(courses => courses.filter(course => course.category == category))
         );
+    }
+
+    saveCourse(courseId: number, changes): Observable<any> {
+        const courses = this.subject.getValue();
+        const courseIndex = courses.findIndex(course => course.id == courseId);
+
+        const newCourses = courses.slice(0);
+
+        // Using js spread operator
+        newCourses[courseIndex] = {
+            ...courses[courseIndex],
+            ...changes
+        };
+
+        // In order to broadcast the changes to all the subscribers, we cannot just change the in memory data.
+        // The subscribers won't be notified
+        // The in memory dat should be immutable, we should create a copy of it and notified the subscribers with all the changes
+        this.subject.next(newCourses);
+
+        return fromPromise(fetch(`/api/courses/${courseId}`, {
+            method: 'PUT',
+            body: JSON.stringify(changes),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }));
     }
 }
--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,18 +1,16 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, Inject, ViewChild} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import {Course} from "../model/course";
 import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 import * as moment from 'moment';
-import {concat, fromEvent} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap} from 'rxjs/operators';
-import {fromPromise} from 'rxjs/internal-compatibility';
+import { Store } from '../common/store.service';
 
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
     styleUrls: ['./course-dialog.component.css']
 })
-export class CourseDialogComponent implements OnInit, AfterViewInit {
+export class CourseDialogComponent {
 
     // Angular provides at the level of the form an Observable that emits the values that are contain in the form
     form: FormGroup;
@@ -25,7 +23,8 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course ) {
+        @Inject(MAT_DIALOG_DATA) course:Course, 
+        private store: Store) {
 
         this.course = course;
 
@@ -38,39 +37,13 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
 
     }
 
-    ngOnInit() {
-        this.form.valueChanges
-            .pipe(
-                filter(() => this.form.valid),
-                // We want to guarantee the values are saved in the same order as they are emmitted
-                // Our safe operations are happening in sequence in the same order we want to trigger them
-                // Completing one first and then the other
-                concatMap(changes => this.saveCourse(changes))
-            )
-            .subscribe()
+    save() {
+        this.store.saveCourse(this.course.id, this.form.value)
+        .subscribe(
+            () => this.close(),
+            err => console.log("Error saving course", err)
+        ) 
     }
-
-    async saveCourse(changes) {
-        fromPromise(fetch(`/api/courses/${this.course.id}`, {
-            method: 'PUT',
-            body: JSON.stringify(changes),
-            headers: {
-                'content-type': 'application/json'
-            }
-        }));
-    }
-
-    ngAfterViewInit() {
-        fromEvent(this.saveButton.nativeElement, 'click')
-        .pipe(
-            // The new values will be ignored as long as the ongoing observable is not completed
-            // We want to manage one click, trigger the save request, and while the safe request is not completed the rest of the clicks will be ignored
-            exhaustMap(() => this.saveCourse(this.form.value))
-        ).subscribe();
-
-    }
-
-
 
     close() {
         this.dialogRef.close();
