43600158f8284a3014d9f3a6760fe70aec39f374
Brais Cidras <braisomary@gmail.com>
2021-12-05 18:04:21 +0100

    The RxJs Debug Operator - Implementation Conclusion

--- src/app/common/debug.ts
+++ src/app/common/debug.ts
@@ -1,11 +1,26 @@
 import { Observable } from "rxjs";
 import { tap } from "rxjs/operators";
 
+export enum RxJsLoggingLevel {
+    TRACE,
+    DEBUG,
+    INFO,
+    ERROR
+}
+
+let rxjsLoggingLevel = RxJsLoggingLevel.INFO;
+
+export function setRxjsLoggingLevel(level: RxJsLoggingLevel) {
+    rxjsLoggingLevel = level;
+}
+
 // Higher order function> function that returns other function
 export const debug = (level: number, message: string) =>
     (source: Observable<any>) => source
         .pipe(
             tap(val => {
-                console.log(message + ": " + val)
+                if (level >= rxjsLoggingLevel) {
+                    console.log(message + ": ", val)
+                }
             })
         )
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -17,6 +17,7 @@ import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
 import { searchLessons } from '../../../server/search-lessons.route';
+import {  debug, RxJsLoggingLevel, setRxjsLoggingLevel } from '../common/debug';
 
 
 @Component({
@@ -39,7 +40,12 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
         this.courseId = this.route.snapshot.params['id'];
-        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`)
+            .pipe(
+                debug(RxJsLoggingLevel.INFO, "course value")
+            );
+
+        setRxjsLoggingLevel(RxJsLoggingLevel.DEBUG);
     }
 
     ngAfterViewInit() {
@@ -47,9 +53,11 @@ export class CourseComponent implements OnInit, AfterViewInit {
             .pipe(
                 map(event => (<HTMLInputElement>(<Event>event).target).value),
                 startWith(''), // It will provide an initial value of string empry to the search parameter
+                debug(RxJsLoggingLevel.TRACE, "search"),
                 debounceTime(400), // Value provided in milliseconds
                 distinctUntilChanged(),
-                switchMap(search => this.loadLessons(search))
+                switchMap(search => this.loadLessons(search)),
+                debug(RxJsLoggingLevel.DEBUG, "lessons value"),
             );
     }
 
