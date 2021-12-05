dfd3398e5e68888ffd84060e936fcf24de46162b
Brais Cidras <braisomary@gmail.com>
2021-12-05 20:24:28 +0100

    The Store Pattern - Loading Initial Data, Selector Methods, Consuming Data

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject, Observable, timer } from "rxjs";
+import { tap, map } from "rxjs/operators";
 import { Course } from "../model/course";
+import { createHttpObservable } from "./util";
 
 // The whole application will access to this service
 @Injectable({
@@ -10,4 +12,30 @@ export class Store {
 
     private subject = new BehaviorSubject<Course[]>([]);
     courses$: Observable<Course[]> = this.subject.asObservable();
+
+    init() {
+        const http$ = createHttpObservable('/api/courses');
+
+        http$.pipe(
+            tap(() => console.log("HTTP Request executed")),
+            map(res => Object.values<Course>(res["payload"]))
+        )
+        .subscribe(
+            courses => this.subject.next(courses)
+        );
+    }
+
+    selectBeginnerCourses(): Observable<Course[]> {
+        return this.filterByCategory('BEGINNER');
+    }
+
+    selectAdvancedCourses(): Observable<Course[]> {
+        return this.filterByCategory('ADVANCED');
+    }
+
+    filterByCategory(category: string): Observable<Course[]> {
+        return this.courses$.pipe(
+            map(courses => courses.filter(course => course.category == category))
+        );
+    }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,30 +20,11 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
-        const http$ = createHttpObservable('/api/courses');
+        const courses$ = this.store.courses$;
+        
+        this.beginnerCourses$ = this.store.selectBeginnerCourses();
 
-        const courses$: Observable<Course[]> = http$.pipe(
-            tap(() => console.log("HTTP Request executed")),
-            map(res => Object.values<Course>(res["payload"])),
-            shareReplay(),
-            // After the time elapse, then retryWhen will subscribe to the http$ observable
-            // and trigger a new http request
-            retryWhen(errors => errors.pipe(
-                /* 
-                    We should use delayWhen NOT delay 
-                    If we use delay, we will delay the whole error stream by a total of 2 seconds
-                    We want after each error to wait fot 2 seconds
-                */
-                delayWhen(() => timer(2000))
-            ))
-        );
-
-        this.beginnerCourses$ = courses$.pipe(
-            map(courses => courses.filter(course => course.category == 'BEGINNER'))
-        );
-        this.advancedCourses$ = courses$.pipe(
-            map(courses => courses.filter(course => course.category == 'ADVANCED'))
-        );
+        this.advancedCourses$ = this.store.selectAdvancedCourses();
     }
 
 }
